2672414ca899e0ac5f231370f64027e066c9e787
Petersweb <32275665+Petersweb@users.noreply.github.com>
2017-11-30 20:21:25 +0000

    Start of php code, new folder for html

--- HTML_STUFF/HTML_funcions.php
+++ HTML_STUFF/HTML_funcions.php
@@ -3,24 +3,15 @@
 /**
  *
  */
-function startHTMML(){
+function startHTMML($css, $pageName){
     $html = <<< START
     <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-55673568-6', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-    <link href="css/main.css" rel="stylesheet" type="text/css">
+  
+    <link href="$css" rel="stylesheet" type="text/css">
     <meta charset="UTF-8">
-    <title>Home | Peters web</title>
+    <title>$$pageName | SITE</title>
 </head>
 START;
 
